a27a7287c2409f1e198d37159452dfc034b5247f
Lehman Black <WileESpaghetti@users.noreply.github.com>
2024-06-25 23:00:16 -0400

    add a controller

---
+++ src/main/java/org/example/controllers/HealthController.java
@@ -0,0 +1,17 @@
+package org.example.controllers;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public HttpStatus index() {
+        return HttpStatus.OK;
+    }
+}
